a395162c11bcbea6a6df75028b48f6583c6e008a
ugodeughi <stefano.corradi@gmail.com>
2021-11-29 13:04:45 +0100

    vine live coding $emit

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,10 +1,11 @@
 <template>
 
   <div>
-
+    <!-- sono in ascolto quando il componente SearchBar scatena levento @sendSearch e lancio la funzione performSearch la quale riceve "dietro le quinte" il parametro textToSearch -->
+    <SearchBar @sendSearch="performSearch"  />
     <div v-if="loaded" class="row" >
       <Character
-        v-for="character in characters"
+        v-for="character in filteredCharacters"
         :key="character.id"
         :character="character"
       />
@@ -22,6 +23,7 @@
 
 import axios from 'axios';
 
+import SearchBar from './SearchBar'
 import Character from './Character';
 import Loader from './Loader'
 
@@ -29,20 +31,44 @@ export default {
   name: 'CharactersList',
   components:{
     Character,
-    Loader
+    Loader,
+    SearchBar
   },
   data(){
     return{
       characters: [],
       loaded:false,
-      apiUrl: 'https://api.sampleapis.com/rickandmorty/characters'
+      apiUrl: 'https://api.sampleapis.com/rickandmorty/characters',
+      textToSearch:''
+    }
+  },
+  computed:{
+    // la computed viene attivata al cambio di un suo dato reattivo
+    // deve avere un return
+    filteredCharacters(){
+      // se non c'è testo da cercare restituisco tutto l'array
+      if(this.textToSearch === ''){
+        return this.characters;
+      }
+      // altrimenti filtro in base al testo da cercare
+      const arrayFiltered = this.characters.filter( item => {
+        // pusho in arrayFiltered l'item solo quando la condizione è vera:
+        // ossia quando item.name include textToSearch
+        return item.name.toUpperCase().includes(this.textToSearch.toUpperCase());
+      });
+      return arrayFiltered;
     }
   },
   methods:{
+    // funzione richiamata dll'evento custom @sendSearch generato con $emit da SearchBar.vue al @click o @keyup
+    performSearch(text){
+      this.textToSearch = text;
+    },
     getApi(){
       axios.get(this.apiUrl)
         .then( r => {
           this.characters = r.data;
+          console.log(this.characters);
           this.loaded = true;
         })
         .catch( e => {
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,48 @@
+/*
+1. genero un evento custom per in inviate il contentuto dell'input
+2. il genitore intercetta l'evento e filtra l'elenco in base al testo inviato
+3. il ciclo for del genitore stamperà il nuovo elenco filtrato
+
+ */
+<template>
+  <div class="row my-5">
+    <div class="col-10">
+      <input
+       v-model="textToSearch"
+       @keyup.enter="$emit('sendSearch',textToSearch)"
+        class="form-control" type="text" placeholder="Riscerca un nome">
+    </div>
+    <div class="col 1">
+      <button
+      @click="$emit('sendSearch',textToSearch)"
+       class="btn btn-info">Cerca</button>
+    </div>
+    <div class="col 1">
+      <button
+      @click="reset"
+       class="btn btn-warning">Reset</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data(){
+    return{
+      textToSearch: ''
+    }
+  },
+  methods:{
+    reset(){
+      this.textToSearch = '';
+      this.$emit('sendSearch',this.textToSearch);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
